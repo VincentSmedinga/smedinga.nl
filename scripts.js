a298949a7d023082180a8b5d81950509a371aa44
Vincent Smedinga <vincent.smedinga@fontoxml.com>
2016-04-22 23:12:14 +0200

    Fire up some JavaScript.

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+	// Remove `no-js` class from <html>
+	var eHtml = document.querySelectorAll('html')[0];
+	eHtml.classList.remove('no-js');
+
+});
